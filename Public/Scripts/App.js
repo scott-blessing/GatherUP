29fda6f43643be27fe11ea6979ed4f9a1bee1d1c
Scott Blessing <scott.blessing@hotmail.com>
2015-03-27 18:45:38 -0500

    Commit all initial files

---
+++ Public/Scripts/App.js
@@ -0,0 +1,16 @@
+﻿angular
+  .module('gatherUPApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'Views/Home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/event', {
+        templateUrl: 'Views/Event.html',
+        controller: 'EventCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  });
