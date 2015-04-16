c8f8760729692e68467c69ac96bd36f5c36928c2
Buttersnips <dhruvvajpeyi@yahoo.co.in>
2015-04-15 23:42:20 -0500

    QuickFix #3
    Added email line to updatePassword

--- Public/updatePassword.php
+++ Public/updatePassword.php
@@ -14,6 +14,7 @@ if ($_POST['password1'] && $_POST['password2'])
 	{
 		$password1 = mysqli_real_escape_string($conn, hash("sha512", $_POST['password1']));
 		$password2 = mysqli_real_escape_string($conn, hash("sha512", $_POST['password2']));
+		$email = mysqli_real_escape_string($conn, $_POST['email']);
 		
 		
 		if ($password1 != $password2){
