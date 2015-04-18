0c27da0ecbd27b5ab75386370dce070779fe8a4b
Buttersnips <dhruvvajpeyi@yahoo.co.in>
2015-04-18 14:21:31 -0500

    Changed to work with vajpeyi2

--- Public/login.php
+++ Public/login.php
@@ -1,10 +1,9 @@
 <?php
 //Variables to be used to connect to MySQL database.
 $host = "engr-cpanel-mysql.engr.illinois.edu"; //Host name
-$username = "gatherup_user"; //Mysql username
+$username = "vajpeyi2_dhruv"; //Mysql username
 $password = "pass(1234)"; //Mysql password
-$db_name = "gatherup_vajpeyi2_gatherup"; //Database name
-$tbl_name = "user"; //Table name
+$db_name = "vajpeyi2_gatherup"; //Database name
 
 //Connect to server and select database.
 $conn = mysqli_connect($host, $username, $password, $db_name) or die("cannot connect server "); 
--- Public/newEvent.php
+++ Public/newEvent.php
@@ -1,9 +1,9 @@
 <?php
 //Variables to be used to connect to MySQL database.
 $host = "engr-cpanel-mysql.engr.illinois.edu"; //Host name
-$username = "gatherup_user"; //Mysql username
+$username = "vajpeyi2_dhruv"; //Mysql username
 $password = "pass(1234)"; //Mysql password
-$db_name = "gatherup_vajpeyi2_gatherup"; //Database name
+$db_name = "vajpeyi2_gatherup"; //Database name
 
 //Connect to server and select database.
 $conn = mysqli_connect($host, $username, $password, $db_name) or die("cannot connect server "); 
