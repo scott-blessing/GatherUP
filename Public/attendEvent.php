932ac280853e77839025eb164c2cb2db5a164519
Buttersnips <dhruvvajpeyi@yahoo.co.in>
2015-04-19 19:24:32 -0500

    AttendEvent fixed for public

--- Public/attendEvent.php
+++ Public/attendEvent.php
@@ -12,8 +12,11 @@ if ($_POST['ID'] && $_POST['email'])
 		// Create an Attends entry with the email and eventid. Add user as Guest
 		$email=mysqli_real_escape_string($conn, $_POST['email']);
 		$id=mysqli_real_escape_string($conn, $_POST['ID']);
-		mysqli_query($conn, "UPDATE Attends SET Status = Status*-1 WHERE EventId=$id AND UserEmail='$email'");
+		$check = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `Attends` WHERE UserEmail='$email' AND EventId=$id"));
+		if($check != NULL)
+			mysqli_query($conn, "UPDATE Attends SET Status = Status*-1 WHERE EventId=$id AND UserEmail='$email'");
+		else
+			mysqli_query($conn, "INSERT INTO Attends (UserEmail, EventId, Carpool, Seats, Status) VALUES ('$email', $id, 0, 0, 1)");
 	}
 mysqli_close($conn);	
-echo(json_encode($data));
 ?>
