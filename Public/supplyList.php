ed1a24f8554a177e21b9f32f11daa93db8772517
ryannk <ryannkelch@gmail.com>
2015-04-21 17:35:52 -0500

    fix

--- Public/supplyList.php
+++ Public/supplyList.php
@@ -16,14 +16,14 @@ if ($_POST['eventid'])
 	{
 		$eventid = mysqli_real_escape_string($conn, $_POST['eventid']);
 		$attendeesQuery = mysqli_query($conn, "SELECT COUNT(UserEmail) AS numAttendees FROM Attends WHERE EventID = $eventid");
-		$numAttendees = mysqli_fetch_array($numAttendees);
+		$numAttendees = mysqli_fetch_array($attendeesQuery);
 		$numAttendees = $numAttendees['numAttendees'];
 
 		$result = mysqli_query($conn, "SELECT S.Name, SC.Quantity, U.Email, U.Name FROM Supplies AS S 
-				INNER JOIN SupplyCounts AS SC ON SC.EventID = S.EventID AND SC.SuppliesName = S.Name 
-					LEFT JOIN Bringing AS B ON S.EventID = B.EventID AND S.Name = B.SuppliesName
-						INNER JOIN User AS U ON B.UserEmail = U.Email
-				WHERE S.EventID=$eventid AND SC.MinAttendees <= $numAttendees AND SC.MaxAttendees >= $numAttendees");
+				INNER JOIN Supply Counts AS SC ON SC.EventID = S.EventID AND SC.SupplyName = S.Name 
+				LEFT JOIN Bringing AS B ON S.EventID = B.EventID AND S.Name = B.SuppliesName
+					INNER JOIN User AS U ON B.UserEmail = U.Email
+				WHERE S.EventID=$eventid AND SC.MinAttendeesToNecessetate <= $numAttendees AND SC.MaxAttendeesToNecessetate >= $numAttendees");
 		$supplies = mysqli_fetch_array($result);
 		while ($supplies != NULL) {
 			array_push($data['supplies'], $supplies);
